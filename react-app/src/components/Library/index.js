f509bae9dde97666ede85aed8b76e08c5bf50e0a
jstnswn <jstnswn@gmail.com>
2022-03-17 11:23:10 -0700

    Add library grid layout

--- react-app/src/components/Library/index.js
+++ react-app/src/components/Library/index.js
@@ -6,8 +6,35 @@ export default function Library() {
     return (
         <div id='library-wrapper'>
             <div className='main-wrapper'>
-                <div id='library-header'></div>
-                <div id='library-body'></div>
+                <div id='library-header'>
+                    <div className='header-image-container'>
+                        <img
+                            alt='library cover'
+                            className='library-header-image'
+                            src='https://cofi-bucket.s3.amazonaws.com/art-seeds/escapade.png'
+                        />
+                    </div>
+                    <h2 className='library-header-title'>Your Collection</h2>
+                </div>
+                <div id='library-body'>
+                    <div className='library-body-header'>
+                        <p className='song-title column-title'>Title</p>
+                        <p className='song-artist column-title'>Artist</p>
+                        <p className='song-album column-title'>Album</p>
+                    </div>
+                    <div className='library-body-container'>
+                        <div className='song-list'>
+                            songs
+                        </div>
+                        <div className='artist-list'>
+                            artists
+                        </div>
+                        <div className='album-list'>
+                            albums
+                        </div>
+                    </div>
+
+                </div>
             </div>
             <LibrarySidebar />
         </div>
