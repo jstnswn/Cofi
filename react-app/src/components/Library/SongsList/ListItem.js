e53fccb552d36f3d746c630b8b9ea3ce84d2566c
jstnswn <jstnswn@gmail.com>
2022-03-17 17:46:46 -0700

    Add dropdown menu for song delete and edit buttons

--- react-app/src/components/Library/SongsList/ListItem.js
+++ react-app/src/components/Library/SongsList/ListItem.js
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { setSong } from '../../../store/active';
 
-export default function ListItem() {
+export default function ListItem({ song, album }) {
+
+    const dispatch = useDispatch();
+    const [hovered, setHovered] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+
+    const playSong = () => {
+        dispatch(setSong(song));
+    };
+
+    const openDropdown = () => setShowMenu(true);
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeDropdown = () => setShowMenu(false);
+
+        const scrollContainer = document.querySelector('.library-body-container');
+
+        document.addEventListener('click', closeDropdown);
+        scrollContainer.addEventListener('scroll', closeDropdown);
+
+        return () => {
+            document.removeEventListener('click', closeDropdown);
+            document.removeEventListener('scroll', closeDropdown);
+        };
+
+    }, [showMenu])
+
     return (
-        <div>ListItem</div>
+        <div
+            className='list-box'
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
+
+            <div className='song-list library-list'>
+                <img alt='cover art' className='list-image' src={song.image_url} /> <span onClick={playSong} className='item'>{song.title}</span>
+            </div>
+            <div className='artist-list library-list'>
+                <p className='item'>{song.artist.name}</p>
+            </div>
+            <div className='album-list library-list'>
+                {album ? <p className='item'>{album.title}</p> : <p className='item'>--</p>}
+
+            </div>
+            <i className={`fa-solid fa-ellipsis song-options ${hovered ? 'active' : ''}`} onClick={openDropdown}></i>
+
+            {showMenu && (
+                <div className='library-list-dropdown'>
+                    <ul>
+                        <li>Edit Song</li>
+                        <li>Delete Song</li>
+                    </ul>
+                </div>
+            )}
+        </div>
     )
 }
--- react-app/src/components/Library/SongsList/SongsList.js
+++ react-app/src/components/Library/SongsList/SongsList.js
@@ -1,85 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { setSong } from '../../../store/active';
+import ListItem from './ListItem';
 
 export default function SongsList({ song }) {
-    const dispatch = useDispatch();
-    const [hovered, setHovered] = useState(false);
-    const [showMenu, setShowMenu] = useState(false);
-
-    const playSong = () => {
-        dispatch(setSong(song));
-    };
-
-    const openDropdown = () => setShowMenu(true);
-
-    useEffect(() => {
-        if (!showMenu) return;
-
-        const closeDropdown = () => setShowMenu(false);
-
-        document.addEventListener('click', closeDropdown);
-
-        return () => document.removeEventListener('click', closeDropdown);
-
-    }, [showMenu])
-
-
-    const songList = song.albums?.length
-        ? (
-            song.albums.map((album, idx) => (
-                <div
-                    key={idx} className='list-box'
-                    onMouseEnter={() => setHovered(true)}
-                    onMouseLeave={() => setHovered(false)}
-                >
-
-                    <div className='song-list library-list'>
-                        <img alt='cover art' className='list-image' src={song.image_url} /> <span onClick={playSong} className='item'>{song.title}</span>
-                    </div>
-                    <div className='artist-list library-list'>
-                        <p className='item'>{song.artist.name}</p>
-                    </div>
-                    <div className='album-list library-list'>
-                        <p className='item'>{album.title}</p>
-                    </div>
-
-                    <i className={`fa-solid fa-ellipsis song-options ${hovered ? 'active' : ''}`} onClick={openDropdown}></i>
-                </div>
-            )))
-        : (
-
-            <div
-                className='list-box'
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-            >
-
-                <div key={song.id} className='song-list library-list'>
-                    <img alt='cover art' className='list-image' src={song.image_url} /> <span onClick={playSong} className='item'>{song.title}</span>
-                </div>
-                <div className='artist-list library-list'>
-                    <p className='item'>{song.artist.name}</p>
-                </div>
-                <div className='album-list library-list'>
-                    --
-                </div>
-
-                <i className={`fa-solid fa-ellipsis song-options ${hovered ? 'active' : ''}`} onClick={openDropdown}></i>
-            </div>
-        )
-
-    return (
-        <>
-            {songList}
-            {showMenu && (
-                <div className='library-list-dropdown'>
-                    <ul>
-                        <li>Edit Song</li>
-                        <li>Delete Song</li>
-                    </ul>
-                </div>
-            )}
-        </>
-    )
+    return song.albums?.length
+            ? (song.albums.map((album, idx) => <ListItem key={idx} song={song} album={album} idx={idx} />))
+            : (<ListItem song={song}/>)
 }
