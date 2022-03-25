8bbd6e31f82603c1ff59eb17d456c18b1ced449d
jstnswn <jstnswn@gmail.com>
2022-03-25 11:19:42 -0700

    Improve library header vertical responsiveness

--- react-app/src/components/MainSidebar/index.js
+++ react-app/src/components/MainSidebar/index.js
@@ -7,17 +7,19 @@ import './MainSidebar.css';
 export default function MainSidebar() {
     return (
         <div id='main-sidebar'>
-            <div>
-                <SongUploadModal />
-                <i className='fas fa-music icon'></i>
-            </div>
-            <div>
-                <AlbumUploadModal />
-                <i className='fad fa-album icon'></i>
-            </div>
-            <div>
-                <PlaylistUploadModal />
-                <i className='fad fa-list-music icon'></i>
+            <div className='content-container'>
+                <div>
+                    <SongUploadModal />
+                    <i className='fas fa-music icon'></i>
+                </div>
+                <div>
+                    <AlbumUploadModal />
+                    <i className='fad fa-album icon'></i>
+                </div>
+                <div>
+                    <PlaylistUploadModal />
+                    <i className='fad fa-list-music icon'></i>
+                </div>
             </div>
         </div>
     )
